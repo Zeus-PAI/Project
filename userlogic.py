99856b8bdd80d2b29d9f1bfce5028928f0ba2d06
RodrigoMartinez10 <ro-martinez11@hotmail.com>
2020-07-19 17:53:13 -0600

    add files

---
+++ userlogic.py
@@ -0,0 +1,38 @@
+from Logic import Logic
+from userobj import UserObj
+
+
+class UserLogic(Logic):
+    def __init__(self):
+        super().__init__()
+        self.keys = ["idUsuario", "Usuario", "nombre_completo", "correo", "contraseña", "Fecha de Nacimiento", "telefono", "pais de residencia", "idTipo"]
+
+    def getUserData(self, user):
+        database = self.get_databaseXObj()
+        sql = f"select * from proyectozeus.usuarios where Usuario='{user}';"
+        data = database.executeQuery(sql)
+        data = self.tupleToDictionaryList(data, self.keys)
+        if len(data) > 0:
+            data_dic = data[0]
+            userObj = UserObj(
+                data_dic["idUsuario"], data_dic["Usuario"], data_dic["nombre_completo"], data_dic["correo"],
+                data_dic["contraseña"], data_dic["Fecha de Nacimiento"], data_dic["telefono"], data_dic["pais de residencia"], data_dic["idTipo"]
+            )
+            return userObj
+        else:
+            return None
+
+
+class RegisterLogic(Logic):
+    def __init__(self):
+        super().__init__()
+
+    def insertNewUser(self, user, nombre_completo, correo, password, Fecha, telefono, pais):
+        database = self.get_databaseXObj()
+        sql = (
+            "insert into proyectozeus.usuarios (`idUsuario`, `Usuario`, `nombre_completo`, `correo`, "
+            + "`contraseña`, `Fecha de Nacimiento`, `telefono`, `pais de residencia`, `idTipo`) "
+            + f"values (0, '{user}', '{nombre_completo}', '{correo}', '{password}', '{Fecha}', '{telefono}', '{pais}', 2);"
+        )
+        rows = database.executeNonQueryRows(sql)
+        return rows
---
+++ userobj.py
@@ -0,0 +1,11 @@
+class UserObj:
+    def __init__(self, id, user, nombre_completo, correo, password, Fecha, telefono, pais, idTipo):
+        self.id = id
+        self.user = user
+        self.name = nombre_completo
+        self.email = correo
+        self.date = Fecha
+        self.phone = telefono
+        self.country = pais
+        self.password = password
+        self.Tipo = idTipo
